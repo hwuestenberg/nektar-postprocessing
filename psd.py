167267817771f85f29cd35eced5379362e88b676
hwustenb <hwustenb@ic.ac.uk>
2025-03-20 17:22:27 +0000

    Streamlined PSD.py and updated inputs similar to draglift.py

--- psd.py
+++ psd.py
@@ -2,7 +2,6 @@
 
 # Matplotlib setup with latex
 import matplotlib.pyplot as plt
-
 params = {'text.usetex': True,
  'font.size' : 10,
 }
@@ -15,99 +14,31 @@ from scipy.signal import welch
 
 from utilities import get_time_step_size, get_label, mser
 from config import directory_names, path_to_directories, dtref, \
-    customMetrics, ref_area, ctu_len, freestream_velocity
+    customMetrics, ref_area, ctu_len, freestream_velocity, save_directory
 
-
-## SCRIPT USER INPUTS
-# Please-work data
-savename = ""
-
+####### SCRIPT USER INPUTS
 # Choose lift or drag
 metric = customMetrics[1]
 
+forces_file = "FWING_TOTAL_forces.fce"
+averaging_len = 10 # [CTU] redundant due to MSER, just use large number
+
+savename = f"PSD-{metric}-{forces_file.split('.')[0]}.pdf"
+savename = save_directory + savename
+
 # Welch parameters
+use_welch = True
 overlap = 2
 windows = 4
 
 xlim = []
-ylim = [1e-16, 1e3]
+ylim = [1e-10, 1e-2]
 
-
-
-
-
-
-# Parse command line arguments                          
-import argparse
-parser = argparse.ArgumentParser()
-parser.add_argument(                                    
-    "avgLen", help="Length of time interval from latest time for sampling average, in CTU", type=float, default=10, nargs='?')
-parser.add_argument(                                    
-    "forces_file", help="File that contains the force data", type=str, default="FWING_TOTAL_forces-process.fce", nargs='?')
-args = parser.parse_args()
 
 
 # Verbose prints
-print("Using forces_file:", args.forces_file)
-print("Averaging over {0} CTUs".format(
-    args.avgLen))
-
-
-
-"""
-    Parv's function
-"""
-def calculate_frequency(time_array, norm: bool, print_message: bool):
-    """!
-    Calculate the frequency of the time signal provided
-    Print the message if needed
-
-    @param time_array (np.array): numpy array with the force trace of the geometry, can be coefficient of lift, or drag, or any integral value
-    @param norm (bool): Whether time_array has been normalised by CTU or not
-    @param print_message (float): To print the sampling dt
-   
-    @return freq (np.float): Sampling frequency of the data. this is typically 1/dt
-    """
-    #Determine the time-step, frequency of force sampling
-    dt = abs(time_array.iloc[1]-time_array.iloc[0]) #Determine the time-step of force sampling
-    if print_message:
-        if norm:
-            print(f"Time step between sampling: {dt} CTUs")
-        else:
-            print(f"Time step between sampling: {dt} secs")
-    freq = 1/dt
-    return freq
-
-
-
-def calculate_psd(sample_trace, frequency_sample, l_ref: float, u_ref: float, sample_division: int, psd_Noverlap: int) :
-    """!
-    Calculate the Strouhal number and the PSD using the input signal provided.
-
-    @param forces_trace (np.array): numpy array with the force trace of the geometry, can be coefficient of lift, or drag, or any integral value
-    @param frequency_sample (np.float): Sampling frequency of the forces data. this is typically 1/dt, where dt is timestep of sample_trace
-    @param l_ref (float): Characteristic length scale of the problem. MOstly the same as value used to normalise for CTU.
-    @param u_ref (float): Reference velocity for the problem. The default is 1.0 for Nektar++
-    @param sample_division (float): Number of divisions in the signal. This influences nperseg, which has an effect on the PSD at low frequencies
-    @param psd_Noverlap (int): Number to divide the nWindow by to calculate the noverlap
-    https://dsp.stackexchange.com/questions/81640/trying-to-understand-the-nperseg-effect-of-welch-method
-    Note: l_ref, u_ref are exposed but always pass normalised arrays
-   
-    @return strouhal_number, psd (np.array): Array of Strouhal number, Power spectral density of x strouhal_number
-    """
-    #Determine the frequency and power spectral density for each sampling point
-   
-    nWindow = round(float(sample_trace.shape[0])/sample_division) #Dividing the length of the sample
-    N_overlap=round(float(nWindow)/psd_Noverlap)
-    print(f"nperseg: {nWindow}, \tnoverlap: {N_overlap}")
-    # Compute PSD using Welch's method
-    frequencies, psd = welch(sample_trace,fs=frequency_sample,nperseg=nWindow)#,
-                             #noverlap=N_overlap)
-    #how to decide nperseg?, has an effect on energy values on low frequency
-    #https://dsp.stackexchange.com/questions/81640/trying-to-understand-the-nperseg-effect-of-welch-method
-    #https://www.osti.gov/biblio/5688766
-    strouhal_number = frequencies*(l_ref/u_ref)
-    return strouhal_number, psd
+print("Using forces_file:", forces_file)
+# print("Averaging over {0} CTUs".format(averaging_len))
 
 
 if __name__ == "__main__":
@@ -118,20 +49,21 @@ if __name__ == "__main__":
     if metric == customMetrics[0]:
         ylabel = r"PSD($C_d$)"
     ax.set_ylabel(ylabel)
-    #ax.ticklabel_format(style='sci',axis='y', scilimits=(0,0), useMathText=True)
     ax.set_xlabel("Strouhal number $St$")
+    #ax.ticklabel_format(style='sci',axis='y', scilimits=(0,0), useMathText=True)
     ax.set_xscale("log")
     ax.set_yscale("log")
+    ax.grid(True, which='both', axis='both')
 
     # Loop all files
     for dirname, dir_color in zip(directory_names, TABLEAU_COLORS):
         # Setup paths
         full_directory_path = path_to_directories + dirname
 
-        forces_file = args.forces_file
-        filename = forces_file.replace("-process", f"-process-overlap-5") # add overlap
+        forces_file = forces_file
+        filename = forces_file.replace(".fce", f"-process-overlap-5.fce") # add overlap
 
-        n_downsamples = [i for i in [1, 2, 5, 10]]
+        n_downsamples = [i for i in [2]]#, 2, 5, 10]]
 
         for n_downsample, downsample_color in zip(n_downsamples, TABLEAU_COLORS):
             print(f"\nProcessing {filename} with downsampling {n_downsample}")
@@ -146,7 +78,7 @@ if __name__ == "__main__":
                 dt = get_time_step_size(full_directory_path)
 
             # Get plot styling
-            label, marker, mfc, ls, color = get_label(full_file_path, dt)
+            label, marker, mfc, ls, color = get_label(full_file_path, dt, color=downsample_color)
             print("Processing {0}...".format(label))
 
             # Read file
@@ -158,11 +90,11 @@ if __name__ == "__main__":
 
             # Mask for given length from final time
             tmax = physTime.max()
-            lowerMask = physTime >= tmax - args.avgLen
+            lowerMask = physTime >= tmax - averaging_len
             upperMask = physTime <= tmax
             mask = (lowerMask == 1) & (upperMask == 1)
             if not np.any(mask):
-                print("No data for interval = [{0}, {1}]".format(tmax - args.avgLen, tmax))
+                print("No data for interval = [{0}, {1}]".format(tmax - averaging_len, tmax))
                 continue
             else:
                 print("Using data on interval = [{0}, {1}]".format(physTime[mask].iloc[0], physTime[mask].iloc[-1]))
@@ -191,18 +123,22 @@ if __name__ == "__main__":
             # timeTransient = physTime.iloc[intTransient]
             # print("End of transient at time {0} CTU and index {1}".format(timeTransient, intTransient))
 
+
             # Get raw sample frequency
             f_sample = 1 / (physTime.iloc[1] - physTime.iloc[0])
             print(f"Raw sample frequency: {f_sample}")
 
-            ## Downsample (
+
+            ## Downsample
+            # However, not for reference data
             # TODO check difference of downsampling BEFORE and AFTER normalisation
-            if n_downsample > 1:
+            if n_downsample > 1 and not "quasi3d" in full_directory_path:
                 metricData = metricData[::n_downsample]
                 physTime = physTime[::n_downsample]
                 f_sample = f_sample / n_downsample
                 print(f"Downsampled sample frequency: {f_sample}")
                 label += f" downsample {n_downsample}"
+
 
             # Normalisation: remove mean
             # TODO check effect of normalisation on PSD
@@ -211,15 +147,18 @@ if __name__ == "__main__":
 
             # Compute FFT using Welch's method
             # Note outputs PSD directly
-            freq_welch, psd_welch = calculate_psd(metricData, f_sample, 1.0, freestream_velocity, windows, overlap)
-            ax.plot(freq_welch, psd_welch, label="Welch " + label, linestyle=ls, color=downsample_color)
+            if use_welch:
+                nperseg = round(float(metricData.shape[0]) / windows)  # Dividing the length of the sample
+                freq_welch, psd_welch = welch(metricData, fs=f_sample, nperseg=nperseg)#, noverlap=overlap)
+                ax.plot(freq_welch, psd_welch, label="Welch " + label, linestyle=ls, color=color)
 
-            # # Compute FFT directly
-            # # Note need to compute PSD via abs()**2
-            # fft_vals = np.fft.rfft(metricData)
-            # fft_freqs = np.fft.rfftfreq(len(metricData), 1 / f_sample)
-            # psd_fft = np.abs(fft_vals) ** 2 / len(metricData)
-            # ax.plot(fft_freqs, psd_fft, label=label, color=downsample_color)
+            # Compute FFT directly
+            # Note need to compute PSD via abs()**2
+            else:
+                fft_vals = np.fft.rfft(metricData)
+                fft_freqs = np.fft.rfftfreq(len(metricData), 1 / f_sample)
+                psd_fft = np.abs(fft_vals) ** 2 / len(metricData)
+                ax.plot(fft_freqs, psd_fft, label="FFT " + label, color=color)
 
 
     ## Aesthetics
@@ -239,19 +178,16 @@ if __name__ == "__main__":
     #ax.plot(xlim, points, label='Reference -25/3', linestyle='dotted', color='black')
 
     # Print reference Strouhal numbers
-    stref = [20, 30, 40, 50, 60]
+    stref = [20, 30, 40, 60, 140, 200]
     for st in stref:
-        ax.plot([st, st], [1e2,1e-16], linestyle='dashed', color='blue', alpha=0.3)
-        ax.text(st, 1e-16, str(st))
+        ax.plot([st, st], [1e5,1e-16], linestyle='dashed', color='blue', alpha=0.3)
+        ax.text(st, 10*ylim[0], str(st))
 
     # Handle legend outside
     handles, labels = ax.get_legend_handles_labels()
     ax.legend(loc='best')
 
-    # if savename:
-    #     if ".pdf" not in savename:
-    #         savename += ".pdf"
-    #     #savename = savename.replace("pdf","png") # switch to png
-    #     fig.savefig(savename, bbox_inches="tight")
+    if savename:
+        fig.savefig(savename, bbox_inches="tight")
 
     plt.show()
